73cccdaf19ad91d5ae6a28605cc5db22a0fb3c9f
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2025-10-24 13:38:51 +0330

    modified

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+    images: {
+    remotePatterns: [new URL('https://ik.imagekit.io/gv5d2avxy/**')],
+  },
 };
 
 export default nextConfig;
