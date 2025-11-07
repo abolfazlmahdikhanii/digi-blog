7244bf6f14df3674ab67cc7b6d207a70348c936a
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2025-11-07 21:39:12 +0330

    update left-sidebar.js

--- components/left-sidebar.js
+++ components/left-sidebar.js
@@ -1,36 +1,61 @@
-
 import React from "react";
 import Link from "next/link";
-import { Home, Library, User, BarChart2, Search } from "lucide-react";
+import {
+  Home,
+  Library,
+  User,
+  BarChart2,
+  Search,
+  Notebook,
+  FileText,
+} from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { useAuth } from "@/context/AuthContext";
+import { useRouter } from "next/router";
+import useFollow from "@/hooks/useFollow";
 const navItems = [
   { href: "/", icon: Home, label: "Home" },
-  { href: "#", icon: Library, label: "Library" },
-  { href: "/profile/johndoe", icon: User, label: "Profile" },
-  { href: "#", icon: BarChart2, label: "Stories" },
-  { href: "#", icon: BarChart2, label: "Stats" },
+  { href: "/me/lists", icon: Library, label: "Library" },
+  { href: "/@", icon: User, label: "Profile" },
+  { href: "/me/stories", icon: FileText, label: "Stories" },
+  // { href: "#", icon: BarChart2, label: "Stats" },
 ];
 
-const following = [
-  { id: "f1", name: "JavaScript in Plain English", avatar: "" },
-  { id: "f2", name: "fatfish", avatar: "" },
-  { id: "f3", name: "Rana Adnan", avatar: "" },
-  { id: "f4", name: "Ahmed Hashesh", avatar: "" },
-];
 const LeftSidebar = () => {
+  const { user } = useAuth();
+  const { follower } = useFollow(user?.username);
+  const router = useRouter();
+
   return (
     <aside className="hidden md:block min-w-72 max-w-72 border-r p-4 py-8">
-      <nav className="flex flex-col gap-1">
+      <nav className="flex flex-col gap-1.5">
         {navItems.map((item) => (
           <Button
             key={item.label}
             variant="ghost"
-            className="justify-start text-base"
+            className={`justify-start text-lg font-normal ${
+              item.label !== "Home" && item.label !== "Profile"
+                ? router.pathname.startsWith(item.href)
+                  ? "text-neutral-100"
+                  : "text-neutral-400"
+                : router.pathname === item.href ||
+                  (item.label === "Profile" && router.pathname.includes("@"))
+                ? "text-neutral-100"
+                : "text-neutral-400"
+            }`}
             asChild
           >
-            <Link href={item.href}>
-              <item.icon className="mr-4 h-6 w-6" />
+            <Link
+              href={
+                item.label !== "Profile"
+                  ? item.href
+                  : user
+                  ? `${item.href}${user?.username}`
+                  : "#"
+              }
+            >
+              <item.icon className="mr-4 h-8 w-8" />
               {item.label}
             </Link>
           </Button>
@@ -39,26 +64,28 @@ const LeftSidebar = () => {
       <div className="mt-8">
         <h3 className="font-semibold text-sm mb-4 px-4">Following</h3>
         <nav className="flex flex-col gap-2">
-          {following.map((user) => (
-            <Button
-              key={user.id}
-              variant="ghost"
-              className="justify-start h-auto"
-              asChild
-            >
-              <Link href="#">
-                <Avatar className="h-6 w-6 mr-3">
-                  <AvatarImage
-                    src={user.avatar.imageUrl}
-                    alt={user.name}
-                    data-ai-hint={user.avatar.imageHint}
-                  />
-                  <AvatarFallback>{user.name.charAt(0)}</AvatarFallback>
-                </Avatar>
-                <span className="truncate">{user.name}</span>
-              </Link>
-            </Button>
-          ))}
+          {follower?.follower && follower?.follower.length > 0 ? (
+            follower.follower.map((userInfo) => (
+              <Button
+                key={userInfo?.follower._id}
+                variant="ghost"
+                className="justify-start h-auto"
+                asChild
+              >
+                <Link href={`/@${userInfo?.follower?.username}`}>
+                  <Avatar className="h-6 w-6 mr-1">
+                    <AvatarImage src={userInfo?.follower.profileImage} alt={userInfo?.follower.name} />
+                    <AvatarFallback className={"capitalize"}>
+                      {userInfo?.follower?.name?.charAt(0)}
+                    </AvatarFallback>
+                  </Avatar>
+                  <span className="truncate">{userInfo?.follower?.name}</span>
+                </Link>
+              </Button>
+            ))
+          ) : (
+            <p>empty</p>
+          )}
         </nav>
       </div>
       <div className="mt-8">
