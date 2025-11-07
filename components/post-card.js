a6d6a9a783aa61a40a588414fd6d184cef1d8766
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2025-11-07 21:39:33 +0330

    update post-card.js

--- components/post-card.js
+++ components/post-card.js
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import Link from "next/link";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 import {
   ThumbsUp,
@@ -14,10 +13,14 @@ import { ImageKitProvider } from "@imagekit/next";
 import { relativeTimeFormat } from "@/lib/utils";
 import { capitalize } from "lodash";
 
+import FollowBtn from "./FollowBtn";
+import HoverProfile from "./hover-profile";
+
 export default function PostCard({
   _id,
   author,
   profileImage,
+  slug,
   createdAt,
   readTime,
   postCover,
@@ -26,51 +29,48 @@ export default function PostCard({
   comments,
   likes,
   category,
+  save,
+  isSave,
 }) {
   return (
     <div className="flex flex-col py-4 border-b ">
       <div className="flex flex-row-reverse sm:flex-row gap-8">
         <div className="flex-1 space-y-3">
-          <Link href={`/profile/@${author?.username}`} className="">
-            <div className="flex items-center gap-2 mb-5">
-              <Avatar className="h-6.5 w-6.5">
-                <AvatarImage
-                  src={author?.profileImage}
-                  alt={author?.name}
-                />
-                <AvatarFallback>
-                  {author?.name?.toUpperCase().charAt(0)}
-                </AvatarFallback>
-              </Avatar>
-              <span className="text-xs font-semibold hover:underline dark:text-neutral-300">
-                {author?.name}
-              </span>
-            </div>
-          </Link>
+          <HoverProfile author={author} />
+
           <Link
-            href={`/post/${_id}`}
+            href={`/@${author?.username}/${slug}`}
             className="hover:text-primary transition-colors "
           >
             <h2 className="font-bold text-xl font-headline tracking-tight">
               {title}
             </h2>
           </Link>
-          <p className="text-muted-foreground text-base mt-2">
+          <p className="text-muted-foreground text-base mt-2 line-clamp-3">
             {shortDescription}
           </p>
           <div className="flex items-center justify-between text-muted-foreground text-sm pt-4">
             <div className="flex items-center gap-5.5">
               <span className="text-xs">{relativeTimeFormat(createdAt)}</span>
 
-              <button className="h-6 w-6 text-muted-foreground hover:text-foreground">
-                <ThumbsUp className="h-3.5 w-3.5" />
-              </button>
-              <div className="flex items-center gap-1">
-                <p className=" text-muted-foreground hover:text-foreground">
-                  <MessageCircle className="h-3.5 w-3.5" />
-                </p>
-                <span className="text-xs">{comments}</span>
-              </div>
+              {likes && !!likes.length > 0 && (
+                <div className="flex items-center gap-1.5">
+                  <p className=" text-muted-foreground hover:text-foreground">
+                    <ThumbsUp className="h-3.5 w-3.5" />
+                  </p>
+                  <span className="text-xs">{likes ? likes.length : 0}</span>
+                </div>
+              )}
+              {comments && !!comments.length > 0 && (
+                <div className="flex items-center gap-1.5">
+                  <p className=" text-muted-foreground hover:text-foreground">
+                    <MessageCircle className="h-3.5 w-3.5" />
+                  </p>
+                  <span className="text-xs">
+                    {comments ? comments.length : 0}
+                  </span>
+                </div>
+              )}
               {category && (
                 <Badge
                   variant="default"
@@ -82,7 +82,15 @@ export default function PostCard({
             </div>
             <div className="flex items-center gap-1">
               <button className="h-8 w-8 text-muted-foreground hover:text-foreground ">
-                <Bookmark className="h-5 w-5" />
+                <Bookmark
+                  className={`h-5 w-5 ${
+                    (save &&
+                      author._id.toString() === save[0]?.userId?.toString()) ||
+                    isSave
+                      ? "fill-blue-600 stroke-blue-600"
+                      : ""
+                  }`}
+                />
               </button>
               <Button
                 variant="ghost"
@@ -94,12 +102,12 @@ export default function PostCard({
             </div>
           </div>
         </div>
-        <div className="sm:w-1/3  flex-shrink-0">
+        <div className="sm:w-1/4  flex-shrink-0">
           <Link href={`/post/${_id}`} className="block">
             <div className="aspect-[4/3] relative max-w-[200px]">
               <ImageKitProvider urlEndpoint="https://ik.imagekit.io/gv5d2avxy">
                 <Image
-                  src={postCover || "/images/placeholder.webp"}
+                  src={postCover?.imageUrl || "/images/placeholder.webp"}
                   alt={title}
                   fill
                   className="object-cover rounded-md"
