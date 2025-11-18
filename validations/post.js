13da3c7cfd9e97bf2aa16fa137787a0d04457628
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2025-11-18 18:06:52 +0330

    update post.js

--- validations/post.js
+++ validations/post.js
@@ -6,9 +6,7 @@ const postSchema = z.object({
   title: z.string().trim().min(2, "Title must be at least 2 characters"),
   content: z.any(), // or z.string() if content is a string
   shortDescription: z.string().trim().min(1, "Description is required"),
-  tags: z.array(z.string()).optional().default([]), // Specify array type
-  postCover: z.string(),
-  status: z.enum(["draft", "published"]).default("draft"), // Better to use enum
+
   readTime: z.number().optional(),
   isShowComment: z.number().optional().default(1),
 });
