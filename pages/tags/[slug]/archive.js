ff823eaf122f7539b00a0046a7315d5b77773f7a
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2025-11-18 18:05:54 +0330

    feat: create archive.js

---
+++ pages/tags/[slug]/archive.js
@@ -0,0 +1,103 @@
+import PostCard from "@/components/post-card";
+import TopicWrapper from "@/components/topic-wrapper";
+import connectToDB from "@/configs/db";
+import postModel from "@/models/posts";
+import topicModel from "@/models/topics";
+import React from "react";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { Spinner } from "@/components/ui/spinner";
+import { ChevronDown } from "lucide-react";
+import ShowMoreBtn from "@/components/show-more-btn";
+
+const Archive = ({ posts, topic }) => {
+  const { data, hasNextPage, isLoading, fetchNextPage, isFetchingNextPage } =
+    useInfiniteQuery({
+      queryKey: ["posts", topic],
+      queryFn: async ({ pageParam }) => {
+        const res = await fetch(
+          `/api/topics/archive?slug=${encodeURIComponent(
+            topic.name
+          )}&page=${pageParam}&limit=10`
+        );
+        if (!res.ok) throw new Error("Failed to fetch stories");
+        return res.json();
+      },
+      getNextPageParam: (lastPage, pages) => {
+        return lastPage.hasMore ? pages.length + 1 : undefined;
+      },
+      initialPageParam: 1,
+    });
+  const allArchive = data?.pages.flatMap((page) => page.posts) || posts || [];
+
+
+  return (
+    <div className="w-11/12 mx-auto">
+      <TopicWrapper
+        title={`Archive of stories in "${topic.name}"`}
+        pageName={"Archive"}
+        isBreadCrumb
+      >
+        <div className="flex flex-col gap-y-8">
+          {allArchive.map((story) => (
+            <PostCard key={story._id} id={story._id} {...story} />
+          ))}
+          <ShowMoreBtn
+            hasNextPage={hasNextPage}
+            dataLength={allArchive.length}
+            isFetchingNextPage={isFetchingNextPage}
+            fetchNextPage={fetchNextPage}
+          />
+        </div>
+      </TopicWrapper>
+    </div>
+  );
+};
+
+export default Archive;
+export async function getServerSideProps(context) {
+  await connectToDB();
+  try {
+    const { slug } = context.query;
+    const { token } = context.req.cookies;
+
+    const topic = await topicModel.findOne({ slug });
+    if (!topic) {
+      return {
+        notFound: true,
+      };
+    }
+    const validToken = verifyToken(token);
+    let currentUser = null;
+    if (validToken) {
+      currentUser = await usersModel.findOne({ email: validToken.email });
+    }
+    // Get all posts for this topic
+    const allPosts = await postModel
+      .find({
+        topics: { $in: [topic._id] },
+        status: "published",
+      })
+      .populate("topics")
+      .populate({ path: "comments" })
+      .populate({ path: "likes" })
+      .populate({ path: "save", match: { userId: currentUser?._id } })
+      .populate("author", "name username")
+      .populate("postCover")
+      .limit(10)
+      .sort({ updatedAt: -1 });
+
+    return {
+      props: {
+        posts: JSON.parse(JSON.stringify(allPosts)),
+        topic: JSON.parse(JSON.stringify(topic)),
+      },
+    };
+  } catch (error) {
+    return {
+      props: {
+        posts: [],
+        topic: [],
+      },
+    };
+  }
+}
